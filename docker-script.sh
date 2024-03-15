3952fbc58b842a2d8aa2cdf1b1740ed2f0f1804c
vishalkaushikdixit <53331357+vishalkaushikdixit@users.noreply.github.com>
2024-03-15 15:04:21 +0530

    Add files via upload

---
+++ docker-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Login to Docker registry (if needed)
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 406749516863.dkr.ecr.ap-south-1.amazonaws.com
+
+# Pull the Docker images
+docker pull 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:lates
+
+# Start your containers using docker-compose
+docker-compose -f /root/docker-compose.yaml up -d
