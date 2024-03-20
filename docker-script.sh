0663d50404edb46a56909d51dcc49aedc055762b
vishalkaushikdixit <53331357+vishalkaushikdixit@users.noreply.github.com>
2024-03-20 19:06:01 +0530

    Update docker-script.sh

--- docker-script.sh
+++ docker-script.sh
@@ -7,6 +7,6 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 docker pull 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:latest
 
 # Start your containers using docker-compose
-cp -rf  /root/script/docker-compose.yaml  /root/docker-compose.yaml
+#cp -rf  /root/script/docker-compose.yaml  /root/docker-compose.yaml
 docker-compose -f /root/docker-compose.yaml up -d
-cp -rf  /root/docker-compose.yaml-commn  /root/docker-compose.yaml
+#cp -rf  /root/docker-compose.yaml-commn  /root/docker-compose.yaml
