eab48f8b748e8eb3c9ababcd440265f01ee2b44b
vishalkaushikdixit <53331357+vishalkaushikdixit@users.noreply.github.com>
2024-04-05 18:15:07 +0530

    Update docker-script.sh

--- docker-script.sh
+++ docker-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+docker image prune -f
 # Login to Docker registry (if needed)
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 406749516863.dkr.ecr.ap-south-1.amazonaws.com
 
@@ -10,4 +10,4 @@ docker pull 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:latest
 #cp -rf  /root/script/docker-compose.yaml  /root/docker-compose.yaml
 docker-compose -f /root/docker-compose.yaml up -d
 #cp -rf  /root/docker-compose.yaml-commn  /root/docker-compose.yaml
-docker image prune -f
+
