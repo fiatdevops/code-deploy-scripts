ea0edccc568db4083e502e96e18ec15c2f9403f8
vishalkaushikdixit <53331357+vishalkaushikdixit@users.noreply.github.com>
2024-04-06 16:43:31 +0530

    Update check-health.sh

--- check-health.sh
+++ check-health.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 MAX_ATTEMPTS=3
-WAIT_TIME=10
+CHECK_INTERVAL=20
+BASE_URL="http://localhost:8000"  
+HEALTH_CHECK_ENDPOINT="/v1/api/health-check/"
 FAILED_ATTEMPTS=0
 
-BASE_URL="http://localhost:8000"
-HEALTH_CHECK_ENDPOINT="/v1/api/health-check/"
+for ((i=1; i<=$MAX_ATTEMPTS; i++)); do
 
-while true; do
- 
     response=$(curl -sSf -w "%{http_code}" "$BASE_URL$HEALTH_CHECK_ENDPOINT" -o /dev/null)
-    
+
     if [ "$response" != "200" ]; then
         ((FAILED_ATTEMPTS++))
-
-        if [ $FAILED_ATTEMPTS -ge $MAX_ATTEMPTS ]; then
-            echo "Application is not healthy after multiple attempts. Reverting deployment..."
-            bash docker-revert.sh
-            exit 1
-        fi
     else
         echo "Application is healthy."
         exit 0
     fi
-
-    sleep $WAIT_TIME
+    sleep $CHECK_INTERVAL
 done
+
+if [ $FAILED_ATTEMPTS -eq $MAX_ATTEMPTS ]; then
+    echo "Application is not healthy after multiple attempts. Running revert script..."
+    bash docker-revert.sh
+    exit 1
+fi
