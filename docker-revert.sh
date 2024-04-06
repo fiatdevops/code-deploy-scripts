3f3c462d9ed4a4ef0472e4e21e5d6d2eb789fd56
vishalkaushikdixit <53331357+vishalkaushikdixit@users.noreply.github.com>
2024-04-06 16:46:43 +0530

    Create docker-revert.sh

---
+++ docker-revert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker tag 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:latestv2 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:latest
+docker-compose -f /root/docker-compose.yaml up -d
