d0216bbc5f4f8e53e54574ea19b9ba60e652e931
fiatdevops <166432919+fiatdevops@users.noreply.github.com>
2024-04-23 07:50:05 +0530

    Update docker-revert.sh

--- docker-revert.sh
+++ docker-revert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker tag 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:latestv2 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe:latest
+docker tag 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe-pre-prod:latestv2 406749516863.dkr.ecr.ap-south-1.amazonaws.com/fiatpe-pre-prod:latest
 docker-compose -f /root/docker-compose.yaml up -d --remove-orphans
 #cp -rf  /root/docker-compose.yaml-commn  /root/docker-compose.yaml
